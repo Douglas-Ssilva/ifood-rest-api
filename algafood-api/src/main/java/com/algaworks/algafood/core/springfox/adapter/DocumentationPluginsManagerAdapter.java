1567afccf70f06f21415a2a5c649e811aec3db1c
Douglas Silva <douglas_d1994@hotmail.com>
2021-09-17 18:22:16 -0300

    Resolvendo conflito de dependências com Spring HATEOAS e SpringFox

--- algafood-api/src/main/java/com/algaworks/algafood/core/springfox/adapter/DocumentationPluginsManagerAdapter.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/springfox/adapter/DocumentationPluginsManagerAdapter.java
@@ -2,11 +2,16 @@ package com.algaworks.algafood.core.springfox.adapter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.plugin.core.PluginRegistry;
+import org.springframework.stereotype.Component;
 
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.DefaultsProviderPlugin;
 import springfox.documentation.spi.service.ResourceGroupingStrategy;
+import springfox.documentation.spi.service.contexts.DocumentationContextBuilder;
+import springfox.documentation.spring.web.SpringGroupingStrategy;
+import springfox.documentation.spring.web.plugins.DefaultConfiguration;
 import springfox.documentation.spring.web.plugins.DocumentationPluginsManager;
 
 /**
@@ -16,9 +21,9 @@ import springfox.documentation.spring.web.plugins.DocumentationPluginsManager;
  *
  */
 
-//@Primary
-//@Component
-//@SuppressWarnings("deprecation")
+@Primary
+@Component
+@SuppressWarnings("deprecation")
 public class DocumentationPluginsManagerAdapter extends DocumentationPluginsManager {
 
 	@Autowired
@@ -29,15 +34,15 @@ public class DocumentationPluginsManagerAdapter extends DocumentationPluginsMana
 	@Qualifier("defaultsProviderPluginRegistry")
 	private PluginRegistry<DefaultsProviderPlugin, DocumentationType> defaultsProviders;
 
-//	@Override
-//	public DocumentationContextBuilder createContextBuilder(DocumentationType documentationType,
-//			DefaultConfiguration defaultConfiguration) {
-//		return defaultsProviders.getPluginOrDefaultFor(documentationType, defaultConfiguration)
-//				.create(documentationType).withResourceGroupingStrategy(resourceGroupingStrategy(documentationType));
-//	}
-//
-//	@Override
-//	public ResourceGroupingStrategy resourceGroupingStrategy(DocumentationType documentationType) {
-//		return resourceGroupingStrategies.getPluginOrDefaultFor(documentationType, new SpringGroupingStrategy());
-//	}
+	@Override
+	public DocumentationContextBuilder createContextBuilder(DocumentationType documentationType,
+			DefaultConfiguration defaultConfiguration) {
+		return defaultsProviders.getPluginOrDefaultFor(documentationType, defaultConfiguration)
+				.create(documentationType).withResourceGroupingStrategy(resourceGroupingStrategy(documentationType));
+	}
+
+	@Override
+	public ResourceGroupingStrategy resourceGroupingStrategy(DocumentationType documentationType) {
+		return resourceGroupingStrategies.getPluginOrDefaultFor(documentationType, new SpringGroupingStrategy());
+	}
 }
